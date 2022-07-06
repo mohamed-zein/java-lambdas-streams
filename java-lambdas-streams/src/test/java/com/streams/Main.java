33e01c5d17a6b6895343f796d17a63a878ba901b
Mohamed Zein <hamadazein84@gmail.com>
2022-07-06 21:20:32 +0000

    Initial main code creating an array and a list

---
+++ java-lambdas-streams/src/test/java/com/streams/Main.java
@@ -0,0 +1,18 @@
+package com.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Integer[] scores = new Integer[] {80, 66, 73, 92, 43};
+
+        List<String> shoppingList = new ArrayList<String>();
+        shoppingList.add("coffee");
+        shoppingList.add("bread");
+        shoppingList.add("pineapple");
+        shoppingList.add("milk");
+        shoppingList.add("pasta");
+    }
+}
