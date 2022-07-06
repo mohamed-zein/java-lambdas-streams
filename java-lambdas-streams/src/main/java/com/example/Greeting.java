21c4111ce45c41a4c40cb6f5aa1e818736f11e30
Mohamed Zein <hamadazein84@gmail.com>
2022-07-06 00:09:27 +0000

    Improve the code docs for the interface

--- java-lambdas-streams/src/main/java/com/example/Greeting.java
+++ java-lambdas-streams/src/main/java/com/example/Greeting.java
@@ -1,6 +1,12 @@
 package com.example ;
 
+/**
+ * This interface is annotated as @FunctionalInterface
+ * Using this annotation make it compiler error to have more than one method 
+ * and so it can be used with lambda notation
+ */
 @FunctionalInterface
 public interface Greeting {
+    
     void sayHello();
 }
