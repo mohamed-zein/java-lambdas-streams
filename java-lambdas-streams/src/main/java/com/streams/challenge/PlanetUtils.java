fa90b236cbefa6c11263e7313b3db1ad6903d066
Mohamed Zein <hamadazein84@gmail.com>
2022-07-07 02:43:08 +0000

    Add solution code to PlanetUtils

--- java-lambdas-streams/src/main/java/com/streams/challenge/PlanetUtils.java
+++ java-lambdas-streams/src/main/java/com/streams/challenge/PlanetUtils.java
@@ -1,21 +1,30 @@
 package com.streams.challenge;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlanetUtils {
     public List<Planet> getPlanetsBeginningWithM(List<Planet> planets) {
-        return null;
+        return planets.stream()
+                .filter(planet -> planet.getName().startsWith("M"))
+                .collect(Collectors.toList());
     }
 
     public List<Planet> getPlanetsWithRings(List<Planet> planets) {
-        return null;
+        return planets.stream()
+                .filter(plantet -> plantet.isHasRings())
+                .collect(Collectors.toList());
     }
 
     public List<Planet> getPlanetsWithMoreThanThreeMoons(List<Planet> planets) {
-        return null;
+        return planets.stream()
+                .filter(planet -> (planet.getNumberOfMoons() > 3))
+                .collect(Collectors.toList());
     }
 
     public List<Planet> getPlanetsWithDensityGreaterThanFive(List<Planet> planets) {
-        return null;
+        return planets.stream()
+                .filter(planet -> (planet.getDensity() > 5))
+                .collect(Collectors.toList());
     }
 }
