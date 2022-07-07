fe25987a146e82ea8a941db68212a6d9089392e6
Mohamed Zein <hamadazein84@gmail.com>
2022-07-07 02:14:12 +0000

    Add challenge code skeleton

---
+++ java-lambdas-streams/src/main/java/com/streams/challenge/PlanetUtils.java
@@ -0,0 +1,21 @@
+package com.streams.challenge;
+
+import java.util.List;
+
+public class PlanetUtils {
+    public List<Planet> getPlanetsBeginningWithM(List<Planet> planets) {
+        return null;
+    }
+
+    public List<Planet> getPlanetsWithRings(List<Planet> planets) {
+        return null;
+    }
+
+    public List<Planet> getPlanetsWithMoreThanThreeMoons(List<Planet> planets) {
+        return null;
+    }
+
+    public List<Planet> getPlanetsWithDensityGreaterThanFive(List<Planet> planets) {
+        return null;
+    }
+}
