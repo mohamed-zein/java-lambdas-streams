df5cefdcff469bb16c69dc431714fcae69e432f5
Mohamed Zein <hamadazein84@gmail.com>
2022-07-07 00:47:11 +0000

    Move the Main class to correct location under src

--- java-lambdas-streams/src/main/java/com/streams/Main.java
+++ java-lambdas-streams/src/main/java/com/streams/Main.java
@@ -3,7 +3,6 @@ package com.streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
